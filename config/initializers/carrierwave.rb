27817d04260c5eb2626579d8c50ea61a4562fc8b
Leandro Maríngolo <leandronsp@gmail.com>
2014-10-22 04:00:44 +0100

    moar heroku setup

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,6 @@ CarrierWave.configure do |config|
   config.root = Settings.carrierwave.root
 end
 
-if Rails.env.development?
+if Rails.env.development? || Rails.env.production?
   CarrierWave.root = 'pictures'
 end
